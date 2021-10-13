0d6e03839fd55146de6d02ff48aece62e2554a8d
Jarad Solomon <jaradsolomon@gmail.com>
2021-10-12 23:41:46 -0500

    CA

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,21 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+
+#     sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS artists (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+#   )
+# SQL
+
+# ActiveRecord::Base.connection.execute(sql)
+
+create_table "students", force: :cascade do |t|
+  t.string "name"
+end
+
   end
 end
